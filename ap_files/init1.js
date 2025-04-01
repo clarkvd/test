d8e16e14eec96b94969d7901741cec2252fc2b6c
Ryan Clark <98057867+clarkvd@users.noreply.github.com>
2025-04-01 14:52:55 -0400

    Add files via upload

---
+++ ap_files/init1.js
@@ -0,0 +1,149 @@
+//<script>
+bouncex.init1Response({
+    "brand_styles": false,
+    "webfonts": false,
+    "cookie": {
+        "swv": false,
+        "did": "3062917375054221422",
+        "vid": 1743526830496823,
+        "v": {
+            "dfp_rblock": "not_ready",
+            "submitted_morning_wire": false,
+            "submitted_afternoon_wire": false,
+            "submitted_sports_wire": false,
+            "submitted_mens_ccb": false,
+            "submitted_women_ccb": false,
+            "submitted_cfb": false,
+            "submitted_photography": false,
+            "submitted_world_of_faith": false,
+            "submitted_ground_game": false,
+            "submitted_breaking_news_alerts": false,
+            "overlay_bid_fill": false
+        },
+        "pdFirstLoad": true,
+        "dg": {
+            "isPreviousCustomer": false,
+            "isSubscriber": false,
+            "isTextSubscriber": false,
+            "cache_ts": 1743523721363
+        },
+        "fvt": 1743523724,
+        "ao": 0,
+        "lp": "https%3A%2F%2Fapnews.com%2F",
+        "as": 0,
+        "vpv": 6,
+        "d": "d",
+        "r": "www.google.com",
+        "cvt": 1743526830,
+        "sid": 17,
+        "gcr": 23,
+        "m": 0,
+        "lvt": 1743526914,
+        "hardID": "2u0iH4tbDgY4IBV1MPNK702QHB0",
+        "softID": "2u0iH48PHIuwTvORwZurNw8q6mG"
+    },
+    "debug": false,
+    "testmode": {
+        "bxtest": false,
+        "office": false,
+        "bxdev": false
+    },
+    "state": {
+        "newvid": false,
+        "iol": false,
+        "tn": 1743526914,
+        "pvid": 6,
+        "gdpr": false,
+        "casl": false,
+        "us": true,
+        "device": {
+            "browser": "Chrome",
+            "version": "134.0.0.0",
+            "platform": "Mac OS X",
+            "device_type": "desktop"
+        },
+        "no_kinesis": true,
+        "request_token": "12802d602dab121eec67466f18da430f15b9e7a2dfa6ffddcb50b53aa5e1691b",
+        "mobile": false,
+        "vip": "134.174.45.198",
+        "geo": {
+            "country": "US",
+            "country_code": "US",
+            "country_code3": "USA",
+            "country_name": "United States",
+            "region": "MA",
+            "region_name": "Massachusetts",
+            "city": "Boston",
+            "postal_code": "02115",
+            "zip": "02115",
+            "latitude": 42.399999999999999,
+            "longitude": -71.099999999999994,
+            "area_code": "",
+            "dma_code": 506,
+            "metro_code": 506,
+            "continent_code": "NA"
+        }
+    },
+    "gbi_stacks": false,
+    "creatives": false,
+    "campaigns": false,
+    "gbi2_enabled": true,
+    "ssp_config": {
+        "index": {
+            "s": "800617",
+            "qa_site_id": 167884,
+            "reload": 300000,
+            "stackWeight": 0,
+            "timeout": 2000,
+            "production": true,
+            "jsonp": true,
+            "ssp_priority": 2
+        },
+        "pbm": {
+            "desktop_id": "980875",
+            "mobile_id": "980876",
+            "publisher_id": "156512",
+            "qa_site_id": "248764",
+            "reload": 300000,
+            "timeout": 2000,
+            "endpoint": "hbopenbid.pubmatic.com\/translator?",
+            "user_sync_endpoint": "ads.pubmatic.com\/AdServer\/js\/user_sync.html",
+            "ssp_priority": 1
+        },
+        "magnite": {
+            "account_id": "20986",
+            "reload": "300000",
+            "site_id": "507294",
+            "zone_id": "3027540",
+            "ssp_priority": 7
+        },
+        "criteo": {
+            "network_id": "11254",
+            "publisher_id": "WKNDdefault",
+            "ssp_priority": 5
+        }
+    },
+    "push_filepath": "\/",
+    "sms_enabled": false,
+    "sms_terms_conditions_link": "",
+    "sms_short_codes": [],
+    "brand_name": "",
+    "ads_config": {
+        "freqCapEnabled": false,
+        "sourcePartner": "none",
+        "cpmSensitivity": "2",
+        "cpmNotes": "",
+        "priorityLevel": "none",
+        "siteLanguage": "none",
+        "newsOrNonNews": "none",
+        "uid2Eligible": false,
+        "dashboardAccessGranted": false,
+        "updatedContractsSigned": false,
+        "iabV3Category": "",
+        "adsGoLiveDate": "2022-06-27T05:00:00.000Z"
+    },
+    "visit_cookie_updates": {
+        "is_go_session": false,
+        "is_gp_session": false
+    }
+});
