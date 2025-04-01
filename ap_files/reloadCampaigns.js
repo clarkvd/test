cde16042215f700c90a7d5860881b47e1e86ff04
Ryan Clark <98057867+clarkvd@users.noreply.github.com>
2025-04-01 14:53:40 -0400

    Add files via upload

---
+++ ap_files/reloadCampaigns.js
@@ -0,0 +1,72 @@
+//<script>
+bouncex.tryCatch(function reloadCampaigns(){
+	var newCampaigns = false;
+	bouncex.creatives = false;
+	bouncex.brandStyles = false;
+	bouncex.webfonts = false;
+
+	if (bouncex.gbi) {
+		bouncex.gbi.stacks = false;
+	}
+
+	var newCookie = {"swv":false,"did":"3062917375054221422","vid":1743526830496823,"v":{"dfp_rblock":"not_ready","submitted_morning_wire":false,"submitted_afternoon_wire":false,"submitted_sports_wire":false,"submitted_mens_ccb":false,"submitted_women_ccb":false,"submitted_cfb":false,"submitted_photography":false,"submitted_world_of_faith":false,"submitted_ground_game":false,"submitted_breaking_news_alerts":false,"overlay_bid_fill":false},"pdFirstLoad":true,"dg":{"isPreviousCustomer":false,"isSubscriber":false,"isTextSubscriber":false,"cache_ts":1743523721363},"fvt":1743523724,"ao":0,"lp":"https%3A%2F%2Fapnews.com%2F","as":0,"vpv":6,"d":"d","r":"www.google.com","cvt":1743526830,"sid":19,"gcr":23,"m":0,"lvt":1743526896,"hardID":"2u0iH4tbDgY4IBV1MPNK702QHB0","softID":"2u0iH48PHIuwTvORwZurNw8q6mG"};
+	var campaignAdded = false;
+	for (var campaignId in newCampaigns) {
+		if (newCampaigns.hasOwnProperty(campaignId)) {
+			// if campaign cookie does not exist
+			if (!bouncex.cookie.campaigns) {
+				bouncex.cookie.campaigns = {};
+			} else {
+				bouncex.cookie.campaigns = Object.assign({}, bouncex.cookie.campaigns);
+			}
+
+			if (!bouncex.cookie.campaigns[campaignId]) {
+				campaignAdded = true;
+				bouncex.cookie.campaigns[campaignId] = {lvt:bouncex.cookie.lvt, vv:0};
+			} else if (newCookie.campaigns[campaignId]) {
+				// need to set campaign cookie's activations_sessions to the new cookie as it gets modified in reloadCampaigns
+				campaignAdded = true;
+				bouncex.cookie.campaigns[campaignId].as = newCookie.campaigns[campaignId].as;
+			}
+		}
+	}
+	if (campaignAdded) {
+		bouncex.setBounceCookie();
+	}
+
+	for (var campaignId in bouncex.campaigns) {
+		if (bouncex.campaigns.hasOwnProperty(campaignId)) { //copy state vars
+			if (newCampaigns[campaignId]) {
+				newCampaigns[campaignId].ap = bouncex.campaigns[campaignId].ap;
+				newCampaigns[campaignId].repressed = Boolean(bouncex.campaigns[campaignId].repressed);
+			}
+
+			if (newCampaigns[campaignId] &&
+				bouncex.campaigns[campaignId].ad_visible &&
+				newCampaigns[campaignId].html.replace(/fsa=(\d+)&|width=(\d+)&|height=(\d+)&/gi,'') == bouncex.campaigns[campaignId].html.replace(/fsa=(\d+)&|width=(\d+)&|height=(\d+)&/gi,'')) {
+								newCampaigns[campaignId] = bouncex.campaigns[campaignId];
+			} else if (newCampaigns[campaignId] && bouncex.isGbi2Campaign(campaignId) && bouncex.campaigns[campaignId].gbi.hasBegunAuction) {
+								newCampaigns[campaignId] = bouncex.campaigns[campaignId];
+			} else {
+				bouncex.destroy_ad(campaignId);
+			}
+		}
+	}
+
+	bouncex.campaigns = newCampaigns;
+	newCampaigns = {};
+
+	bouncex.debug = false;
+		bouncex.setBounceCookie();
+	if (bouncex.campaigns) {
+		for (var campaignId in bouncex.campaigns) {
+			if (bouncex.campaigns[campaignId].ad_visible && typeof bouncex.repressCampaigns === 'function') {
+				bouncex.repressCampaigns(campaignId);
+			}
+		}
+		bouncex.loadBounceCss(bouncex.initActivationFuncs);
+	}
+		bouncex.loadBrandStyles();
+	bouncex.loadWebfonts();
+
+	})();
